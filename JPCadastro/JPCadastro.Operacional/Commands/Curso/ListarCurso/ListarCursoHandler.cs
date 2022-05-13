684f27e9c2f1b400d5c0cac421f4a13a33d8d2ee
malcorinte alcorinte <mauricio@ispac.com.br>
2022-05-13 15:10:43 -0300

    Finalizado os Cursos

---
+++ JPCadastro/JPCadastro.Operacional/Commands/Curso/ListarCurso/ListarCursoHandler.cs
@@ -0,0 +1,33 @@
+﻿using JPCadastro.Core.DTOs;
+using JPCadastro.Operacional.Interfaces.Repositories;
+using MediatR;
+using prmToolkit.NotificationPattern;
+
+namespace JPCadastro.Operacional.Commands.Curso.ListarCurso
+{
+    public class ListarCursoHandler : Notifiable,
+        IRequestHandler<ListarCursoRequest, CommandResponse>
+    {
+        private readonly IRepositoryCurso _repositoryCurso;
+
+        public ListarCursoHandler(IRepositoryCurso repositoryCurso)
+        {
+            _repositoryCurso=repositoryCurso;
+        }
+
+        public Task<CommandResponse> Handle(ListarCursoRequest request,
+            CancellationToken cancellationToken)
+        {
+            //TESTANDO SE O Curso JÁ ESTA CADASTRADO
+            var colecaoCurso = _repositoryCurso.ListarSemRastreamento();
+
+            return Task.FromResult(new CommandResponse(colecaoCurso.Select(p => new ListarCursoResponse
+            {
+               Id= p.Id,
+               Nome= p.Nome,
+               Periodo=  p.Periodo,
+               ProfessorId= p.ProfessorId
+            }), this)); ;
+        }
+    }
+}
